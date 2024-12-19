7267dec4c549b1cde79ebc8789268e17ae8f20a6
Sebastiano Tronto <sebastiano@tronto.net>
2024-12-19 07:21:47 +0100

    Improved part 2 day 19 2024 with string_view

--- 2024/19/day19b-string.cpp
+++ 2024/19/day19b-string.cpp
@@ -11,27 +11,28 @@
 #include <vector>
 using namespace std;
 
-vector<string_view> gettowels(const string_view& line) {
-	vector<string_view> ret;
-	unsigned sz = line.size();
-	for (int i = 0, j = 0; j != -1; i = j+2) {
+vector<string> gettowels(const string& line) {
+	vector<string> ret;
+	int j = 0;
+	for (int i = 0; j != -1; i = j+2) {
 		j = line.find(",", i);
-		ret.push_back(line.substr(i, (j == -1 ? sz : j) - i));
+		ret.push_back(line.substr(i, (j == -1 ? line.size() : j)-i));
 	}
 	return ret;
 }
 
-int64_t f(const string_view& p, const vector<string_view>& towels, vector<int64_t>& mem) {
-	unsigned lp = p.size();
-	if (lp == 0) return 1;
-	if (mem[lp] != -1) return mem[lp];
+int64_t countpossible(const string& p, const vector<string>& towels, vector<int64_t>& mem) {
+	if (p.size() == 0)
+		return 1;
+	if (mem[p.size()] != -1)
+		return mem[p.size()];
 	int64_t ret = 0;
 	for (auto t : towels) {
-		unsigned lt = t.size();
-		if (t == p.substr(0, lt))
-			ret += f(p.substr(lt, lp - lt), towels, mem);
+		if (t == p.substr(0, t.size()))
+			ret += countpossible(p.substr(
+			    t.size(), p.size() - t.size()), towels, mem);
 	}
-	return (mem[lp] = ret);
+	return (mem[p.size()] = ret);
 }
 
 int main() {
@@ -46,7 +47,7 @@ int main() {
 	int64_t count = 0;
 	for (auto p : patterns) {
 		for (auto& m : mem) m = -1;
-		count += f(p, towels, mem);
+		count += countpossible(p, towels, mem);
 	}
 	cout << count << endl;
 	return 0;
