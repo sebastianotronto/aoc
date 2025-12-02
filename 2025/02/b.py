c0c0f809bb5b2327db96c7dbb5e9ef8ea5960b00
Sebastiano Tronto <sebastiano@tronto.net>
2025-12-02 06:15:47 +0100

    Day 2 2025 (solved part 2 before part 1 because I misread lol)

--- 2025/02/b.py
+++ 2025/02/b.py
@@ -1,7 +1,16 @@
 import fileinput
 
+def invk(n, k):
+	for i in range(0, len(n)-k, k):
+		if n[i:i+k] != n[i+k:i+2*k]:
+			return False
+	return True
+
 def invalid(n):
-	return n[:len(n)//2] == n[len(n)//2:]
+	for k in range(1, len(n)//2 + 1):
+		if invk(n, k):
+			return True
+	return False
 
 s = 0
 with fileinput.input() as lines:
@@ -10,7 +19,6 @@ with fileinput.input() as lines:
 			l = r.split("-");
 			for n in range(int(l[0]), int(l[1])+1):
 				if invalid(str(n)):
-					print(n)
 					s += n
 print(s)
 				
